adc2c971876aa2ec3c79f32c90f5330116e7c4c5
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 10:31:08 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -1,13 +1,11 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
-    // Calculate the global index of the current thread
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int blockStartIndex  = blockIdx.x * blockDim.x * N;
+    int threadStartIndex = blockStartIndex + (threadIdx.x * N);
+    int threadEndIndex   = threadStartIndex + N;
+    int i;
 
-    // Use a stride to allow threads to handle the entire vector
-    int stride = blockDim.x * gridDim.x;
-
-    // Each thread processes multiple elements spaced apart by the grid stride
-    for (int i = index; i < N; i += stride) {
+    for( i=threadStartIndex; i<threadEndIndex; ++i ){
         C[i] = A[i] + B[i];
     }
 }
