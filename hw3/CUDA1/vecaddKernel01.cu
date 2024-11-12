033b81baa61c928ee96e274010a39f19b2b56224
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 12:51:18 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -3,7 +3,7 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
-    printf("%d\n", index);
+    // printf("%d\n", index);
     // Loop with stride of N
     for (int i = index; i < N * N; i += N) {
         C[i] = A[i] + B[i];
