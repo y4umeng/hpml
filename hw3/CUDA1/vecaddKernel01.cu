51ec0a8620d60410e8bcf7a26452ffcfc2fd1be6
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 13:11:23 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
+// __global__ void AddVectors(const float* A, const float* B, float* C, int N)
+// {
+//     int blockStartIndex  = blockIdx.x * blockDim.x * N;
+//     int threadStartIndex = blockStartIndex + threadIdx.x;
+//     int threadEndIndex   = threadStartIndex + blockDim.x * N;
+//     int i;
+
+//     for( i=threadStartIndex; i<=threadEndIndex; i+=N ){
+//         C[i] = A[i] + B[i];
+//     }
+// }
+
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
-    int blockStartIndex  = blockIdx.x * blockDim.x * N;
-    int threadStartIndex = blockStartIndex + threadIdx.x;
-    int threadEndIndex   = threadStartIndex + blockDim.x * N;
-    int i;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for( i=threadStartIndex; i<=threadEndIndex; i+=N ){
-        C[i] = A[i] + B[i];
+    // Loop through each value assigned to this thread
+    for (int i = 0; i < N; i += blockDim.x * gridDim.x) {
+        int idx = index + i;
+        if (idx < N) {  // Boundary check
+            C[idx] = A[idx] + B[idx];
+        }
     }
 }
-
