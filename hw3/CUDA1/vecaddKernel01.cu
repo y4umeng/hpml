d415cf3483fb874b7d15ebdc9b150eeff967ea9c
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 10:57:26 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -1,7 +1,7 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int blockStartIndex  = blockIdx.x * blockDim.x * N;
-    int blockEndIndex = blackStartIndex + 32 * N;
+    int blockEndIndex = blockStartIndex + 32 * N;
     // int threadStartIndex = blockStartIndex + (threadIdx.x * N);
     // int threadEndIndex   = threadStartIndex + N;
     int i;
