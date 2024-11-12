aa9e50a52c445777aca75313acb197d4d221aa7a
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 13:14:32 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -1,26 +1,13 @@
 #include <stdio.h>
 
-// __global__ void AddVectors(const float* A, const float* B, float* C, int N)
-// {
-//     int blockStartIndex  = blockIdx.x * blockDim.x * N;
-//     int threadStartIndex = blockStartIndex + threadIdx.x;
-//     int threadEndIndex   = threadStartIndex + blockDim.x * N;
-//     int i;
-
-//     for( i=threadStartIndex; i<=threadEndIndex; i+=N ){
-//         C[i] = A[i] + B[i];
-//     }
-// }
-
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int blockStartIndex  = blockIdx.x * blockDim.x * N;
+    int threadStartIndex = blockStartIndex + (threadIdx.x * N);
+    int threadEndIndex   = threadStartIndex + N;
+    int i;
 
-    // Loop through each value assigned to this thread
-    for (int i = 0; i < N; i += blockDim.x * gridDim.x) {
-        int idx = index + i;
-        if (idx < N) {  // Boundary check
-            C[idx] = A[idx] + B[idx];
-        }
+    for( i=threadStartIndex; i<threadEndIndex; ++i ){
+        C[i] = A[i] + B[i] - 1;
     }
 }
