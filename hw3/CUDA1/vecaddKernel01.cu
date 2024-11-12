df05443135c3dfe18dd8fee1828c42e837cf2006
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 10:27:39 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -2,9 +2,6 @@ __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-    // Make sure we don't access out-of-bounds elements
-    if (index < N) {
-        C[index] = A[index] + B[index];
-    }
+    C[index] = A[index] + B[index];
 }
 
