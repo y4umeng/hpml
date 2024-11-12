3221f31b96fbc33d76b65eb73cc9e3a17f501511
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-11-12 10:22:53 -0500

    moving to colab

--- hw3/CUDA1/vecaddKernel01.cu
+++ hw3/CUDA1/vecaddKernel01.cu
@@ -1,24 +1,10 @@
-///
-/// vecAddKernel00.cu
-/// For COMS E6998 Spring 2023
-/// Instructor: Parajit Dube and Kaoutar El Maghraoui
-/// Based on code from the CUDA Programming Guide
-/// By David Newman
-/// Created: 2011-02-16
-/// Last Modified: 2011-02-16 DVN
-///
-/// This Kernel adds two Vectors A and B in C on GPU
-/// without using coalesced memory access.
-/// 
-
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
-    int blockStartIndex  = blockIdx.x * blockDim.x * N;
-    int threadStartIndex = blockStartIndex + (threadIdx.x * N);
-    int threadEndIndex   = threadStartIndex + N;
-    int i;
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-    for( i=threadStartIndex; i<threadEndIndex; ++i ){
-        C[i] = A[i] + B[i];
+    // Make sure we don't access out-of-bounds elements
+    if (index < N) {
+        C[index] = A[index] + B[index];
     }
 }
+
