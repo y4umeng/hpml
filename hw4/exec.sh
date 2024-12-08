b203ecc55a9f07e88a705687cdd2d0cf459daa2d
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-12-07 19:16:54 -0500

    hw4

--- hw4/exec.sh
+++ hw4/exec.sh
@@ -5,14 +5,12 @@ python main.py --batch_size 128 --epochs 2
 python main.py --batch_size 512 --epochs 2
 python main.py --batch_size 2048 --epochs 2
 
-# Q2 (only have access to two GPUs)
+# Q2 & Q3 (only have access to two GPUs)
 
 python main.py --batch_size 32 --epochs 2 --gpu_ids 0 1
 python main.py --batch_size 128 --epochs 2 --gpu_ids 0 1
 python main.py --batch_size 512 --epochs 2 --gpu_ids 0 1
 python main.py --batch_size 2048 --epochs 2 --gpu_ids 0 1
-
-# Q3
 
 # Q4
 
